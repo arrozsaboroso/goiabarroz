bc13705f17b3f926e0259f56fbf74e996063c987
arrozsaboroso <128633973+arrozsaboroso@users.noreply.github.com>
2024-07-29 15:50:21 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,18 +5,39 @@ const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 const perguntas = [
     {
-        enunciado: "Pergunta 1",
+        enunciado: "Pergunta 1",Você decide o seu futuro 
+        Você acabou de sair de seu curso e percebe que tem apenas 6 reias no bolso e você esta com muita fome e,
+         tendo que escolher entre uma goiaba e a passagem do onibus.?Qual você escolhe? 
+    
         alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
+            "Alternativa 1",passagem de onibus
+            "Alternativa 2"Goiaba
         ]
     },
     {
-        enunciado: "Pergunta 2",
+        enunciado: "Pergunta 2",No momento me que você foi comer a goiaba tranquilamente,
+         no final da rua você avista 2 caras e eles acabam te chamando para conversar um assunto...
         alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
+            "Alternativa 1",movido pela sua curiosidade, você vai conversar com eles para saber do que se trata
+            "Alternativa 2",Você os ignora e vai pelo caminho contrário.
         ]
     }
+    const perguntas = [
+        {
+            enunciado: "Pergunta 3",
+            alternativas: [
+                "Alternativa 1",
+                "Alternativa 2"
+            ]
+        },
+        {
+            enunciado: "Pergunta 4",
+            alternativas: [
+                "Alternativa 1",
+                "Alternativa 2"
+            ]
+        }
+    ];
+    
 ]; 
 
